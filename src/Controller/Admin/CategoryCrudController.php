f2c70e88b217fb6576ac46a76ccc359a0c5445b0
Mehdi <akamimehdi.dev@gmail.com>
2024-04-25 02:20:33 +0200

    Ajouter le contrôleur CRUD pour l'entité Category dans l'administration

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Catégorie')
+            ->setEntityLabelInPlural('Catégories')
+        ;
+    }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('Titre')->setHelp("Titre de la catégorie"),
+            SlugField::new('slug')->setLabel('URL')->setTargetFieldName('name')->setHelp("URL de votre catégorie générée automatiquement"),
+        
+           
+        ];
+    }
+}
