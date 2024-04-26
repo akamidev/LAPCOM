09d25ee55a5e9a8746f84652b83ad855079c3453
Mehdi <akamimehdi.dev@gmail.com>
2024-04-26 22:41:38 +0200

    Ajouter le contrôleur ProductController pour la gestion des produits

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{      
+    
+
+    #[Route('/produit/{slug}', name: 'app_product')]
+    public function index($slug, ProductRepository $productRepository): Response
+    {   
+        $product = $productRepository->findOneBySlug($slug);
+        
+
+        return $this->render('product/index.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
