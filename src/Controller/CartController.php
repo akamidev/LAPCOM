b61276a4c94269fe3c28ab23c02733eb0742bfd8
Mehdi <akamimehdi.dev@gmail.com>
2024-04-27 05:56:32 +0200

    Ajouter les méthodes decrease et remove à la classe Cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Classe\Cart;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,24 +16,47 @@ class CartController extends AbstractController
     {
         return $this->render('cart/index.html.twig', [
 
-       'cart' => $cart->getCart()     
+            'cart' => $cart->getCart()
         ]);
     }
 
     #[Route('/cart/add/{id}', name: 'app_cart_add')]
-    public function add($id, Cart $cart, ProductRepository $productRepository): Response
-    {
+    public function add($id, Cart $cart, ProductRepository $productRepository, Request $request): Response
+    {   
+        
+
         $product = $productRepository->findOneById($id);
 
         $cart->add($product);
-        
+
         $this->addFlash(
             'success',
             'Votre produit a bien été ajouté au panier.'
         );
 
-        return $this->redirectToRoute('app_product', [
-            'slug' => $product->getSlug()
-        ]);
+        return $this->redirect($request->headers->get('referer'));
+    }
+    
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
+    public function decrease($id, Cart $cart): Response
+    {   
+        
+
+        $cart->decrease($id);
+
+        $this->addFlash(
+            'success',
+            'Votre produit a bien été suprimée de votre panier.'
+        );
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove', name: 'app_cart_remove')]
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+
+        return $this->redirectToRoute('app_home');
     }
 }
