a0632ef59a6f39cdf084e8c1633ff8387c201461
Mehdi <akamimehdi.dev@gmail.com>
2024-04-27 05:55:08 +0200

    Ajouter les méthodes decrease et remove à la classe Cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -26,6 +26,26 @@ class Cart
     
         $this->requestStack->getSession()->set('cart', $cart);
     }
+public function decrease($id)
+    {
+    
+    
+        $cart = $this->requestStack->getSession()->get('cart');
+
+       
+            if ($cart[$id]['qty'] > 1) {
+                $cart[$id]['qty'] = $cart[$id]['qty'] -1;
+            } else {
+                unset($cart[$id]);
+            }
+            $this->requestStack->getSession()->set('cart', $cart);
+        
+
+    }
+    public function remove()
+    {
+        return $this->requestStack->getSession()->remove('cart');
+    }  
 
     public function getCart()
     {
