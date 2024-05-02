f7107bda9a2fdf6b6fc21d4d3d2a791aa7965fb0
Mehdi <akamimehdi.dev@gmail.com>
2024-05-02 04:25:08 +0200

    Ajouter le formulaire de commande dans la classe OrderType.php

---
+++ src/Form/OrderType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use App\Entity\Carrier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {           
+    
+                    
+        $builder
+
+            ->add('addresses', EntityType::class, [
+                'label' => "Choisissez votre adresse de livraison",
+                'required' => true,
+                'class' => Address::class,
+                'expanded' => true,
+                'choices' => $options['addresses'],
+                'label_html' => true
+            ])
+
+           
+
+            ->add('carriers', EntityType::class, [
+                'label' => "Choisissez votre transporteur",
+                'required' => true,
+                'class' => Carrier::class,
+                'expanded' => true,
+                'label_html' => true
+
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => "Valider ma commande",
+                'attr' => [
+                    'class' => 'btn btn-success w-100'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'addresses' => null
+        ]);
+    }
+}
