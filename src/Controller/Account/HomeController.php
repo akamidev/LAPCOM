b3a8495a488914c1d688a43347c0cd69abf5000d
Mehdi <akamimehdi.dev@gmail.com>
2024-05-06 00:28:35 +0200

    chore: Mettre à jour le contrôleur HomeController pour afficher les commandes de l'utilisateur dans la page de compte

--- src/Controller/Account/HomeController.php
+++ src/Controller/Account/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Account;
 
 
+use App\Repository\OrderRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +14,19 @@ class HomeController extends AbstractController
    
 
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepository): Response
     {
-        return $this->render('account/index.html.twig');
+        
+        $orders = $orderRepository->findBy([
+        
+           'user' => $this->getUser(),
+           'state'=> [2,3]
+        ]);
+        // dd($orders);
+        return $this->render('account/index.html.twig',[
+
+            'orders' => $orders
+        ]);
     }
 
     
