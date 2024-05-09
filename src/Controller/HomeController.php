fc2b42df861de54e8fe84846a6095ca235e33331
Mehdi <akamimehdi.dev@gmail.com>
2024-05-09 20:28:11 +0200

    Update HomeController to fetch headers and products for homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Classe\Mail;
+use App\Repository\HeaderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(HeaderRepository $headerRepository, ProductRepository $productRepository): Response
+    { 
+  
+        return $this->render('home/index.html.twig',[
+
+            'headers' => $headerRepository->findAll(),
+            'productsInHomepage' => $productRepository->findByIsHomepage(true),
+        ]);
     }
 }
