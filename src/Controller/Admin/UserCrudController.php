f6d19edee4b9b2e7768cacc4c263c1171fb2d93f
Mehdi <akamimehdi.dev@gmail.com>
2024-05-25 03:36:48 +0200

    Update UserCrudController to include last login date field

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -28,8 +29,9 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('lastname')->setLabel('Nom'),
             TextField::new('firstname')->setLabel('Prénom'),
+            TextField::new('lastname')->setLabel('Nom'),
+            DateField::new('lastLoginAt')->setLabel('Dernière connexion')->onlyOnIndex(),
             ChoiceField::new('roles')->setLabel('Permissions')->setHelp('Vous pouvez choisir le role de cet utilisateur')->setChoices([
                 'ROLE_USER' => 'ROLE_USER',
                 'ROLE_ADMIN' => 'ROLE_ADMIN',
