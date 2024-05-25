713ea2b89fbf47cf8f4d8c9c27dc60110960e175
Mehdi <akamimehdi.dev@gmail.com>
2024-05-25 03:33:13 +0200

    Update HomeController to include search functionality

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Controller;
 
 use App\Classe\Mail;
+use App\Form\SearchType;
 use App\Repository\HeaderRepository;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,13 +13,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HeaderRepository $headerRepository, ProductRepository $productRepository): Response
+    public function index(Request $request, HeaderRepository $headerRepository, ProductRepository $productRepository): Response
     { 
-  
-        return $this->render('home/index.html.twig',[
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+        
 
+        $products = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $query = $form->get('query')->getData();
+            $products = $productRepository->findByName($query);
+        }
+        $searchPerformed = $form->isSubmitted() && $form->isValid();
+        return $this->render('home/index.html.twig',[
+            'form' => $form->createView(),
+            'products' => $products,
             'headers' => $headerRepository->findAll(),
             'productsInHomepage' => $productRepository->findByIsHomepage(true),
+            'searchPerformed' => $searchPerformed,
         ]);
     }
+
+
 }
