6f98bfb8baf5ea585a6099e847de14667dcfe230
Mehdi <akamimehdi.dev@gmail.com>
2024-06-08 10:08:30 +0200

    chore: Add validation constraints for contact form fields

--- src/Form/ContactMessageType.php
+++ src/Form/ContactMessageType.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\ContactMessage;
@@ -10,16 +9,68 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ContactMessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('subject', TextType::class)
-            ->add('message', TextareaType::class)
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le nom ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le nom ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z\s]+$/',
+                        'message' => 'Le nom ne peut contenir que des lettres et des espaces.',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'L\'email ne peut pas être vide.',
+                    ]),
+                    new Assert\Email([
+                        'message' => 'Veuillez entrer une adresse email valide.',
+                    ]),
+                ],
+            ])
+            ->add('subject', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le sujet ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 100,
+                        'maxMessage' => 'Le sujet ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s\p{P}]+$/u',
+                        'message' => 'Le sujet ne peut contenir que des lettres, des chiffres et des signes de ponctuation.',
+                    ]),
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le message ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 1000,
+                        'maxMessage' => 'Le message ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[\s\S]*$/',
+                        'message' => 'Le message contient des caractères non valides.',
+                    ]),
+                ],
+            ])
             ->add('save', SubmitType::class, ['label' => 'Envoyer Message']);
     }
 
