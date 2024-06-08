7dd4409a159f4e742eb518ed2c4090f871fb4c84
Mehdi <akamimehdi.dev@gmail.com>
2024-06-08 04:46:43 +0200

    chore: Add validation constraints for firstname and lastname fields in RegisterUserType form

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\User;
@@ -7,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -20,38 +20,44 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
                 'constraints' => [
-                
+                    new NotBlank([
+                        'message' => 'Merci de saisir votre prénom',
+                    ]),
                     new Length([
                         'min' => 2,
                         'max' => 40,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z]+$/',
+                        'message' => 'Le prénom ne doit contenir que des lettres.',
                     ]),
                 ],
                 'attr' => ['placeholder' => 'Merci de saisir votre prénom']
             ])
-
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
                 'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de saisir votre nom',
+                    ]),
                     new Length([
                         'min' => 2,
                         'max' => 40,
                     ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z]+$/',
+                        'message' => 'Le nom ne doit contenir que des lettres.',
+                    ]),
                 ],
-
                 'attr' => ['placeholder' => 'Merci de saisir votre nom']
             ])
-
             ->add('email', EmailType::class, [
                 'label' => 'Votre adresse email',
                 'attr' => ['placeholder' => 'Merci de saisir votre adresse email']
             ])
-
-
-// plainPassword sera un champ qui ne sera pas lié à l'entité User
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'constraints' => [
@@ -67,15 +73,14 @@ class RegisterUserType extends AbstractType
                 'first_options' => [
                     'label' => 'Votre mot de passe',
                     'attr' => ['placeholder' => 'Merci de saisir votre mot de passe'],
-                    'hash_property_path' => 'password', // permet de hasher le mot de passe
+                    'hash_property_path' => 'password',
                 ],
                 'second_options' => [
-                    'label' => 'Confirmer votre mot de passe ',
+                    'label' => 'Confirmer votre mot de passe',
                     'attr' => ['placeholder' => 'Merci de confirmer votre mot de passe'],
                 ],
-                'mapped' => false, // essaie de ne pas faire le liens entre l'entity et le champs que je te donne 
+                'mapped' => false,
             ])
-
             ->add('submit', SubmitType::class, [
                 'label' => 'S\'inscrire',
                 'attr' => ['class' => 'btn btn-success']
@@ -85,7 +90,6 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            //les contrainte permet de controller les formulaire et assurer leurs securité 
             'constraints' => [
                 new UniqueEntity([
                     'fields' => 'email',
