55a4af90391733220f5eaf24615a8ed9befced4a
Mehdi <akamimehdi.dev@gmail.com>
2024-06-08 04:46:03 +0200

    chore: Refactor CategoryController to improve code readability and performance

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,28 +1,45 @@
 <?php
 
+
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route('/categorie/{slug}', name: 'app_category')]
-    public function index($slug, CategoryRepository $CategoryRepository): Response
+    public function index($slug, CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
     {
-
-        $category = $CategoryRepository->findOneBySlug($slug);
-
+        $category = $categoryRepository->findOneBySlug($slug);
         if (!$category) {
-
             return $this->redirectToRoute('app_home');
         }
 
+        $products = $productRepository->findBy(['category' => $category]);
 
         return $this->render('category/index.html.twig', [
             'category' => $category,
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/navbar', name: 'app_navbar')]
+    public function navbar(CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        $productsByCategory = [];
+
+        foreach ($categories as $category) {
+            $productsByCategory[$category->getId()] = $productRepository->findBy(['category' => $category]);
+        }
+
+        return $this->render('partials/_navbar.html.twig', [
+            'categories' => $categories,
+            'productsByCategory' => $productsByCategory,
         ]);
     }
 }
